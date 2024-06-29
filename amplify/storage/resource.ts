251690fff2ca883512ae618eba748e9996c7c769
unknown <1bryanvalenzuela@gmail.com>
2024-06-28 22:45:36 -0400

    bugfix

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,35 +1,6 @@
-import { type ClientSchema, a, defineData, defineStorage } from "@aws-amplify/backend";
+import { defineStorage } from "@aws-amplify/backend";
 
 // STORAGE
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
-});
-
-// SCHEMAS
-const schema = a.schema({
-  Juego: a
-    .model({
-      name: a.string(),
-      content: a.string(),
-    })
-  .authorization((allow) => [allow.publicApiKey()]),
-  Comentario: a
-  .model({
-    name: a.string(),
-    content: a.string(),
-    likes: a.integer(),
-  })
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
 });
